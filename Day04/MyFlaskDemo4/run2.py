abe06e4da54fc679c2f9e9acc8dbf8d6c3613261
PengLovol <1816525677@qq.com>
2019-10-10 22:01:00 +0800

    add

--- Day04/MyFlaskDemo4/run2.py
+++ Day04/MyFlaskDemo4/run2.py
@@ -1,22 +1,23 @@
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
 from flask_sqlalchemy import SQLAlchemy
-
 import pymysql
 pymysql.install_as_MySQLdb()
 
-app = Flask(__name__)
+app=Flask(__name__)
 #指定连接字符串
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:120913@localhost:3306/flask'
+
 #指定让SQLAlchemy自动追踪程序的修改
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-#指定执行完操作之后自动提交
+
+#指定执行完操作之后自动提交 ==db.session.commit()
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 #为当前的项目创建一个SQLAlchemy的实例
-db = SQLAlchemy(app)
+db=SQLAlchemy(app)
 
 
-# 创建模型类 - Models
+#创建模型类－Models
 # 创建 Users 类,映射到表中叫 users 表
 # 创建字段 : id , 主键,自增
 # 创建字段 : username , 长度为80的字符串,不允许为空,必须唯一
@@ -37,78 +38,74 @@ class Users(db.Model):
     def __repr__(self):
         return '<Users:%r>' % self.username
 
-
 class Student(db.Model):
-    __tablename__ = "student"
-    id = db.Column(db.Integer,primary_key=True)
-    sname = db.Column(db.String(30),nullable=False)
-    sage = db.Column(db.Integer)
-
+    __tablename__='student'
+    id=db.Column(db.Integer,primary_key=True)
+    sname=db.Column(db.String(30),nullable=False)
+    sage=db.Column(db.Integer)
     def __init__(self,sname,sage):
-        self.sname = sname
-        self.sage = sage
+        self.sage=sage
+        self.sname=sname
 
     def __repr__(self):
         return '<Student %r>' % self.sname
 
 class Teacher(db.Model):
-    __tablename__ = "teacher"
-    id = db.Column(db.Integer,primary_key=True)
-    tname = db.Column(db.String(30),nullable=False)
-    tage = db.Column(db.Integer)
-
+    __tablename__='teacher'
+    id=db.Column(db.Integer,primary_key=True)
+    tname=db.Column(db.String(30))
+    tage=db.Column(db.Integer)
     def __init__(self,tname,tage):
-        self.tname = tname
-        self.tage = tage
-
+        self.tname=tname
+        self.tage=tage
     def __repr__(self):
         return "<Teacher %r>" % self.tname
 
 class Course(db.Model):
-    __tablename__ = "course"
-    id = db.Column(db.Integer,primary_key=True)
-    cname = db.Column(db.String(30))
-
+    __tablename__='course'
+    id=db.Column(db.Integer,primary_key=True)
+    cname=db.Column(db.String(30))
     def __init__(self,cname):
-        self.cname = cname
-
+        self.cname=cname
     def __repr__(self):
         return "<Course %r>" % self.cname
 
 
 
 # 将创建好的实体类映射回数据库
-# db.drop_all()
 db.create_all()
 
-
+#
 @app.route('/insert')
 def insert_views():
     # 创建 Users 对象
-    users = Users('王伟超',38,'wangwc@163.com')
+    users = Users('王可',24,'wangk@163.com')
     # 将对象通过db.session.add()插入到数据库
     db.session.add(users)
     # 提交插入操作
     db.session.commit()
     return "Insert Success"
-
 @app.route('/register',methods=['GET','POST'])
 def register():
-    if request.method == 'GET':
-        return render_template('02-register.html')
+    if request.method=='GET':
+        return render_template('02_register.html')
     else:
         # 接收前端传递过来的数据
-        username = request.form.get('username')
-        age = int(request.form.get('age'))
-        email = request.form.get('email')
+        username=request.form.get('username')
+        age=int(request.form.get('age'))
+        email=request.form.get('email')
         # 将数据构建成 Users 对象
-        users = Users(username,age,email)
-        # 通过 db.session.add 将对象保存进数据库
+        users=Users(username,age,email)
+        # 将对象通过db.session.add()插入到数据库
         db.session.add(users)
-        # 提交
-        # db.session.commit()
+        # 提交插入操作
+        # db.session.commit() ==app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
         return "Register OK"
 
 
-if __name__ == "__main__":
+
+
+
+
+if __name__=='__main__':
     app.run(debug=True)
