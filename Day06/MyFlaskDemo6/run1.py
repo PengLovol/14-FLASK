62e0797176403ee9ae0f23bae579ae9863fe043d
PengLovol <1816525677@qq.com>
2019-10-14 19:31:52 +0800

    add

--- Day06/MyFlaskDemo6/run1.py
+++ Day06/MyFlaskDemo6/run1.py
@@ -84,12 +84,41 @@ class Wife(db.Model):
     wage=db.Column(db.Integer)
     # 增加一个列:表示引用自teacher表的主键
     teacher_id=db.Column(db.Integer,db.ForeignKey('teacher.id'))
-    def __init__(self,wname,wage,):
+    def __init__(self,wname,wage):
         self.wname=wname
         self.wage = wage
 
     def __repr__(self):
         return "<Wife %r>" % self.wage
+
+#新增student表 ，实现多对多关系：课程和学生是多对多关系
+class Student(db.Model):
+    __tablename__ = "student"
+    id = db.Column(db.Integer,primary_key=True)
+    sname = db.Column(db.String(30))
+    # 增加关联属性以及反向引用
+    courses=db.relationship('Course',
+                            secondary='student_course',
+                            lazy='dynamic',
+                            backref=db.backref(
+                                'students',
+                                lazy='dynamic'
+                            )
+                            )
+
+
+    def __init__(self,sname):
+        self.sname = sname
+
+    def __repr__(self):
+        return "<Student %r>" % self.sname
+#新增student_course表：关联表
+student_course=db.Table(
+    'student_course',
+    db.Column('id',db.Integer,primary_key=True),
+    db.Column('student_id',db.Integer,db.ForeignKey('student.id')),
+    db.Column('course_id',db.Integer,db.ForeignKey('course.id'))
+)
 
 
 # 将创建好的实体类映射回数据库
@@ -376,6 +405,73 @@ def query_wife():
     return "Query OK"
 
 #练习3.png
+@app.route('/register_wife',methods=['GET','POST'])
+def register_wife():
+    if request.method=='GET':
+        #查询Teacher列表,发送到模板上
+        teachers = Teacher.query.all()
+        return render_template('06_register_wife.html',params=locals())
+    else:
+        # 接收 teacher 的 value
+        teacher_id=request.form['teacher']
+        wife=Wife.query.filter_by(teacher_id=teacher_id).first()
+        if wife:
+            errMsg="EXIST"
+            # 判断 wife 表中的 teacher_id 列是否已经有了value的值
+            teachers = Teacher.query.all()
+            return render_template('06_register_wife.html',params=locals())
+        else:
+            # 接收剩余数据
+            wname=request.form['wname']
+            wage=request.form['wage']
+            # 根据teacher_id查询teacher对象
+            wife_teacher=Teacher.query.filter_by(id=teacher_id).first()
+            # 创建wife对象并保存回数据库
+            wife_new=Wife(wname,wage)
+            wife_new.teacher=wife_teacher
+            db.session.add(wife_new)
+            return redirect('/show_all_wife')
+
+#显示左右wife信息
+@app.route('/show_all_wife')
+def show_all_wife():
+    allWife=Wife.query.all()
+    return render_template('06_show_all_wife.html',params=locals())
+#增加删除功能
+@app.route('/wife_delete')
+def wife_delete():
+    wife_id=request.args.get('id')
+    wife=Wife.query.filter_by(id=wife_id).first()
+    db.session.delete(wife)
+    return redirect('/show_all_wife')
+
+
+#多对多：往关联表中插入数据
+@app.route('/add_student_course')
+def add_student_course():
+    # 查询 张三丰 的信息
+    stu = Student.query.filter_by(sname='陈敏').first()
+    # 查询 PYTHON基础 的信息
+    cour = Course.query.filter_by(id=3).first()
+    # 将cour课程追加到stu的courses列表中
+    stu.courses.append(cour)
+    db.session.add(stu)
+    return "Add OK"
+
+#查询
+@app.route('/query_student_course')
+def query_student_course():
+    # 查询陈敏所选的所有课程
+    student = Student.query.filter_by(id=1).first()
+    courses = student.courses.all()
+    print('学员姓名:%s' % student.sname)
+    for cour in courses:
+        print('所选课程:%s' % cour.cname)
+
+    return "Query OK"
+
+
+
 
 
 
